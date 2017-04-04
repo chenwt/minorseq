a1b43c554d403eb81ff6bc66504d7bb0ebac0719
Armin Töpfer <atoepfer@pacificbiosciences.com>
2017-04-04 23:23:55 +0200

    Define each identifier in a dedicated statement.

--- src/Fisher.cpp
+++ src/Fisher.cpp
@@ -103,7 +103,9 @@ double Fisher::gammln(double xx)
 {
     static double cof[6] = {76.18009172947146,  -86.50532032941677,    24.01409824083091,
                             -1.231739572450155, 0.1208650973866179e-2, -0.5395239384953e-5};
-    double x, tmp, ser;
+    double x;
+    double tmp;
+    double ser;
     int j;
 
     x = xx - 1.0;
