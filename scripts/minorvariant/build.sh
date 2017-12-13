ce6f6e338fd22a44d6ab1fc8f108eb92e9a38d60
David Seifert <dseifert@pacificbiosciences.com>
2017-12-13 21:52:47 +0100

    Use default version of individual modules
    * Makes future dependency management easier and less cumbersome

--- scripts/minorvariant/build.sh
+++ scripts/minorvariant/build.sh
@@ -2,7 +2,7 @@
 set -euo pipefail
 
 source /mnt/software/Modules/current/init/bash
-module load cram/0.7 samtools smrttools/incremental
+module load cram samtools smrttools/incremental
 
 export PATH=`pwd`/artifacts/minorseq:$PATH
 
